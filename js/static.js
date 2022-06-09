ee8fcbde40776bb516ac4097fef55f8176419a02
maustiptop100 <maustiptop100@gmail.com>
2022-06-09 16:32:16 +0200

    09-06-2022 16-32

--- js/static.js
+++ js/static.js
@@ -1,6 +1,8 @@
 const track = (id, callback) => {
     $.getJSON('/tracks/tracks.json', (data) => {
-        callback(data[id]);
+        $.getJSON('/tracks/' + data.tracks[id] + '/track.json', d => {
+            callback(d, '/tracks/' + data.tracks[id] + '/track.mp3')
+        })
     });
 }
 
@@ -8,17 +10,17 @@ const maps_tag = (track) => {
     return '<iframe id="map-frame" style="border:0; width: 425px; height: 250px" loading="lazy" allowFullScreen referrerPolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB69POiA-b8gISRO23cIiTOW6pcvO6om6U&q='+track.google_maps_place_id+'"></iframe>';
 }
 
-const sound_tag = (track) => {
+const sound_tag = (sound_path) => {
     return '<div class="audio" id="audio-wrapper">\n' +
-        '            <audio controls id="audio" style="float: left; width: 425px;"><source src="'+track.sound+'" type="audio/mpeg"></audio>\n' +
+        '            <audio controls id="audio" style="float: left; width: 425px;"><source src="'+sound_path+'" type="audio/mpeg"></audio>\n' +
         '        </div>';
 }
 
 const load_track = (track_id) => {
-    track(track_id, (track) => {
+    track(track_id, (track, sound_path) => {
         $("#title").html(track.name);
         $("#author").html(track.author);
-        $("#sound").html(sound_tag(track));
+        $("#sound").html(sound_tag(sound_path));
         $("#map").html(maps_tag(track));
         $("#description").html(track.description);
 
@@ -28,8 +30,10 @@ const load_track = (track_id) => {
 
 const tracks = (callback) => {
     $.getJSON('/tracks/tracks.json', (data) => {
-        $.each(data, (id, value) => {
-            callback(id, value);
+        $.each(data.tracks, (id, value) => {
+            $.getJSON('/tracks/' + value + '/track.json', d => {
+                callback(id, d)
+            })
         });
     });
 };
